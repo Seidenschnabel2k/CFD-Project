32dbea75a9e5c2d3a6858aa142cccde934c9caf9
Nils Rieser <nils@rieser.tech>
2025-10-20 14:17:41 +0200

    new file:   Allrun10 	new file:   refineMesh.py 	new file:   system/streamlinesPoints

---
+++ refineMesh.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import os
+import asyncio
+import shutil
+from pathlib import Path
+
+
+mesh_step = [5, 8, 10, 12]
+
+
+async def main():
+    procs = []
+    for i in mesh_step:
+        shutil.copytree("../CFD_Labor_Group1/",
+                        f"{i}", dirs_exist_ok=True,
+                        ignore=shutil.ignore_patterns('.git'))
+        os.environ['yNet'] = str(i * 15)
+        os.environ['xNet1'] = str(i * 5)
+        os.environ['xNet2'] = str(i * 1)
+        os.environ['xNet3'] = str(i * 20)
+        p = await asyncio.create_subprocess_exec(f"{i}/Allrun")
+        procs.append(p)
+
+    await asyncio.gather(*(p.wait() for p in procs))
+
+asyncio.run(main())
