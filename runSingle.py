9ab1b63f9db52783d65d996e001196525ebe854a
Nils Rieser <nils@rieser.tech>
2025-11-11 19:03:31 +0100

    Automated cellsize for inlet + fix for lower wall (limit to 1 L)

--- runSingle.py
+++ runSingle.py
@@ -18,13 +18,16 @@ i = 14
 de = 1/i
 er = de/ds
 n = find_N(ds, de, er, 1)
+ni = find_N(ds, de, er, .2)
 
 
-os.environ['yNet'] = str(i * 15 + n)
+os.environ['yNet'] = str(i * 14 + n)
 os.environ['xNet1'] = str(i * 5)
-os.environ['xNet2'] = str(i * 1)
+os.environ['xNet2'] = str(int(i * .6)+ni*2)
 os.environ['xNet3'] = str(i * 20)
 os.environ['wallNCell'] = str(n)
 os.environ['wallER'] = str(er)
+os.environ['wallER_inv'] = str(1/er)
 
+os.system("echo $xNet2")
 os.system("./Allrun")
